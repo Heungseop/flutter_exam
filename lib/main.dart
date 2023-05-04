cb842af696791ac671cdaf184f75e9533f399e4c
Heungseop <imt300@naver.com>
2023-05-04 11:48:31 +0900

    3.0 Header

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,40 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("appbar ")),
-        body: Center(
-          child: Text(
-            'hello',
-          ),
-        ),
-      ),
+          backgroundColor: Color(0xff181818),
+          body: SafeArea(
+              child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          "Hey Selena",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        Text(
+                          "Welcome back",
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.5),
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          ))),
     );
   }
 }
