489b587e1071ea1e0d7eaa9fdd1ab710375247e0
Heungseop <imt300@naver.com>
2023-05-04 13:24:49 +0900

    3.2 Buttons Section

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,11 @@ class App extends StatelessWidget {
           backgroundColor: Color(0xff181818),
           body: SafeArea(
               child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 40),
+            padding: EdgeInsets.symmetric(
+              horizontal: 40,
+            ),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
@@ -37,6 +40,48 @@ class App extends StatelessWidget {
                           ),
                         ),
                       ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 120,
+                ),
+                Text(
+                  "Total Balance",
+                  style: TextStyle(
+                      color: Colors.white.withOpacity(0.8), fontSize: 22),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "\$5 194 482",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 44,
+                      fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(45)),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 50,
+                        ),
+                        child: Text(
+                          "Transfer",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
                     )
                   ],
                 )
